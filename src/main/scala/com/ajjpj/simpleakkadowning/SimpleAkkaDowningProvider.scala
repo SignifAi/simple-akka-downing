430b7f27e729962ad0ff2aacede4d0814d727d7f
Bruno Mahé <bruno@signifai.io>
2018-03-21 17:00:37 -0700

    Improve implementation

--- src/main/scala/com/ajjpj/simpleakkadowning/SimpleAkkaDowningProvider.scala
+++ src/main/scala/com/ajjpj/simpleakkadowning/SimpleAkkaDowningProvider.scala
@@ -1,114 +1,122 @@
 package com.ajjpj.simpleakkadowning
 
-import akka.actor.{Actor, ActorLogging, ActorSystem, Cancellable, Props}
+import akka.actor.{ Actor, ActorLogging, ActorSystem, Cancellable, Props }
 import akka.cluster.ClusterEvent._
-import akka.cluster.{Cluster, DowningProvider, MemberStatus}
-import com.ajjpj.simpleakkadowning.SurvivalDecider.{ClusterMemberInfo, ClusterState}
+import akka.cluster.{ Cluster, DowningProvider, MemberStatus }
+import com.ajjpj.simpleakkadowning.SurvivalDecider.{ ClusterMemberInfo, ClusterState }
 
-import scala.concurrent.duration.{Duration, FiniteDuration}
+import scala.concurrent.duration.{ Duration, FiniteDuration }
 
 /**
-  * This DowningProvider implementation is the entry point to the SimpleAkkaDowning library. Configuring it as
-  *  akka.cluster.downing-provider-class activates it.
-  */
+ * This DowningProvider implementation is the entry point to the SimpleAkkaDowning library. Configuring it as
+ *  akka.cluster.downing-provider-class activates it.
+ */
 class SimpleAkkaDowningProvider(system: ActorSystem) extends DowningProvider {
-  import Helpers._
+	import Helpers._
 
-  private val cc = system.settings.config.getConfig("simple-akka-downing")
+	private val cc = system.settings.config.getConfig("simple-akka-downing")
 
-  override val downRemovalMargin: FiniteDuration = {
-    val key = "down-removal-margin"
-    toRootLowerCase(cc.getString(key)) match {
-      case "off" => Duration.Zero
-      case _     => cc.getMillisDuration(key) requiring (_ >= Duration.Zero, key + " > 0s, or off")
-    }
-  }
+	override val downRemovalMargin: FiniteDuration = {
+		val key = "down-removal-margin"
+		toRootLowerCase(cc.getString(key)) match {
+			case "off" => Duration.Zero
+			case _ => cc.getMillisDuration(key) requiring (_ >= Duration.Zero, key + " > 0s, or off")
+		}
+	}
 
-  override def downingActorProps = {
-      import Helpers._
-      val cc = system.settings.config.getConfig("simple-akka-downing")
-      val key = "stable-after"
-      val stableInterval = cc.getMillisDuration(key) requiring (_ > Duration.Zero, key + " > 0s")
+	override def downingActorProps = {
+		import Helpers._
+		val cc = system.settings.config.getConfig("simple-akka-downing")
+		val key = "stable-after"
+		val stableInterval = cc.getMillisDuration(key) requiring (_ > Duration.Zero, key + " > 0s")
 
-      val decider = SurvivalDecider(system.settings.config)
+		val decider = SurvivalDecider(system.settings.config)
 
-      Some (Props(new DowningActor(stableInterval, decider)))
-  }
+		Some(Props(new DowningActor(stableInterval, decider)))
+	}
 }
 
 private[simpleakkadowning] class DowningActor(stableInterval: FiniteDuration, decider: SurvivalDecider) extends Actor with ActorLogging {
-  case class SplitBrainDetected(clusterState: ClusterState)
+	case class SplitBrainDetected(clusterState: ClusterState)
 
-  private var state: ClusterState = _
+	private var state: ClusterState = _
 
-  private val cluster = Cluster.get(context.system)
-  cluster.subscribe(self, classOf[ClusterDomainEvent])
+	private val cluster = Cluster.get(context.system)
+	cluster.subscribe(self, classOf[ClusterDomainEvent])
 
+	private var unreachableTimer = Option.empty[Cancellable]
 
-  private var unreachableTimer = Option.empty[Cancellable]
+	override def receive = {
+		// cluster.state may or may not reflect the changes during event handling, so we need to keep track of cluster state ourselves
 
-  override def receive = {
-    // cluster.state may or may not reflect the changes during event handling, so we need to keep track of cluster state ourselves
+		case msg @ CurrentClusterState(members, unreachable, _, _, _) =>
+			log.debug("State: {} - Msg: {}", state, msg)
+			val upMembers = members.filter(_.status == MemberStatus.Up).map(m => ClusterMemberInfo(m.address, m.roles, m))
+			state = ClusterState(upMembers, unreachable.map(_.uniqueAddress.address))
+			triggerTimer()
+		case msg @ MemberUp(m) =>
+			log.debug("State: {} - Msg: {}", state, msg)
+			//TODO is there a good and meaningful way to count 'weaklyUp' members?
+			state = state.copy(
+				upMembers = state.upMembers + ClusterMemberInfo(m.address, m.roles, m),
+				unreachable = state.unreachable - m.address)
+			triggerTimer()
+		case msg @ MemberLeft(m) =>
+			log.debug("State: {} - Msg: {}", state, msg)
+			state = state.copy(
+				upMembers = state.upMembers.filterNot(_.uniqueAddress == m.address),
+				unreachable = state.unreachable - m.address)
+			triggerTimer()
+		case msg @ ReachableMember(m) =>
+			log.debug("State: {} - Msg: {}", state, msg)
+			state = state.copy(
+				upMembers = state.upMembers + ClusterMemberInfo(m.address, m.roles, m),
+				unreachable = state.unreachable - m.address)
+			triggerTimer()
+		case msg @ UnreachableMember(m) =>
+			log.debug("State: {} - Msg: {}", state, msg)
+			//      println ("******* UNREACHABLE: " + m.uniqueAddress.address.port)
 
-    case msg @ CurrentClusterState(members, unreachable, _, _, _) =>
-    	log.debug("State: {} - Msg: {}", state, msg)
-      val upMembers = members.filter(_.status == MemberStatus.Up).map(m => ClusterMemberInfo(m.uniqueAddress, m.roles, m))
-      state = ClusterState(upMembers, unreachable.map(_.uniqueAddress))
-      triggerTimer()
-    case msg @ MemberUp(m) =>
-    	log.debug("State: {} - Msg: {}", state, msg)
-      //TODO is there a good and meaningful way to count 'weaklyUp' members?
-      state = state.copy(upMembers = state.upMembers + ClusterMemberInfo(m.uniqueAddress, m.roles, m))
-      triggerTimer()
-    case msg @ MemberLeft(m) =>
-    	log.debug("State: {} - Msg: {}", state, msg)
-      state = state.copy(upMembers = state.upMembers.filterNot (_.uniqueAddress == m.uniqueAddress))
-      triggerTimer()
-    case msg @ ReachableMember(m) =>
-    	log.debug("State: {} - Msg: {}", state, msg)
-      state = state.copy (unreachable =  state.unreachable - m.uniqueAddress)
-      triggerTimer()
-    case msg @ UnreachableMember(m) =>
-    	log.debug("State: {} - Msg: {}", state, msg)
-//      println ("******* UNREACHABLE: " + m.uniqueAddress.address.port)
+			state = state.copy(
+				upMembers = state.upMembers.filterNot(_.uniqueAddress == m.address),
+				unreachable = state.unreachable + m.address)
 
-      state = state.copy (unreachable = state.unreachable + m.uniqueAddress)
-      triggerTimer()
+			triggerTimer()
 
-    case SplitBrainDetected(clusterState) if decider.isInMinority(clusterState, cluster.selfAddress) =>
-//      println ("########################### " + clusterState.upMembers + " |||||||||||||||||||| " + clusterState.unreachable + " >>>>>>>>>>>>>>>>>>> " + clusterState.upUnreachable)
-    	log.debug("Cluster state: {}", clusterState)
-      log.error("Network partition detected. I am not in the surviving partition --> terminating")
-      context.system.terminate()
-      context.become(Actor.emptyBehavior)
-    case SplitBrainDetected(clusterState) if iAmResponsibleAction(clusterState) =>
-//      println ("########################### " + clusterState.upMembers + " |||||||||||||||||||| " + clusterState.unreachable + " >>>>>>>>>>>>>>>>>>> " + clusterState.upUnreachable)
-      log.warning("Network partition detected. I am the responsible node in the surviving partition --> terminating unreachable nodes {}", cluster.state.unreachable)
-      cluster.state.unreachable.foreach(m => cluster.down(m.address))
-    case SplitBrainDetected(clusterState) =>
-//      println ("########################### " + clusterState.upMembers + " |||||||||||||||||||| " + clusterState.unreachable + " >>>>>>>>>>>>>>>>>>> " + clusterState.upUnreachable)
-      log.info("Network partition detected. I am in the surviving partition, but I am not the responsible node, so nothing needs to be done")
-  }
+		case SplitBrainDetected(clusterState) if decider.isInMinority(clusterState, cluster.selfAddress) =>
+			//      println ("########################### " + clusterState.upMembers + " |||||||||||||||||||| " + clusterState.unreachable + " >>>>>>>>>>>>>>>>>>> " + clusterState.upUnreachable)
+			log.debug("Cluster state: {}", clusterState)
+			log.error("Network partition detected. I am not in the surviving partition --> terminating")
+			context.system.terminate()
+			context.become(Actor.emptyBehavior)
+		case SplitBrainDetected(clusterState) if iAmResponsibleAction(clusterState) =>
+			//      println ("########################### " + clusterState.upMembers + " |||||||||||||||||||| " + clusterState.unreachable + " >>>>>>>>>>>>>>>>>>> " + clusterState.upUnreachable)
+			log.warning("Network partition detected. I am the responsible node in the surviving partition --> terminating unreachable nodes {}", cluster.state.unreachable)
+			cluster.state.unreachable.foreach(m => cluster.down(m.address))
+		case SplitBrainDetected(clusterState) =>
+			//      println ("########################### " + clusterState.upMembers + " |||||||||||||||||||| " + clusterState.unreachable + " >>>>>>>>>>>>>>>>>>> " + clusterState.upUnreachable)
+			log.info("Network partition detected. I am in the surviving partition, but I am not the responsible node, so nothing needs to be done")
+	}
 
-  private def triggerTimer(): Unit = {
-    unreachableTimer.foreach(_.cancel())
-    unreachableTimer = None
+	private def triggerTimer(): Unit = {
+		unreachableTimer.foreach(_.cancel())
+		unreachableTimer = None
 
-    if (state.unreachable.nonEmpty) {
-      import context.dispatcher
-      // Store the cluster's state in the message to ensure split brain detection is done based on the state that was stable.
-      //  If the handler reads the then-current cluster state, that may have changed between the scheduler firing and the event
-      //  being handled
-      unreachableTimer = Some(context.system.scheduler.scheduleOnce(stableInterval, self, SplitBrainDetected(state)))
-    }
-  }
+		if (state.unreachable.nonEmpty) {
+			import context.dispatcher
+			// Store the cluster's state in the message to ensure split brain detection is done based on the state that was stable.
+			//  If the handler reads the then-current cluster state, that may have changed between the scheduler firing and the event
+			//  being handled
+			unreachableTimer = Some(context.system.scheduler.scheduleOnce(stableInterval, self, SplitBrainDetected(state)))
+		}
+	}
 
-  private def iAmResponsibleAction(clusterState: ClusterState) = {
-    clusterState.sortedUpAndReachable.head.uniqueAddress.address == cluster.selfAddress
-  }
+	private def iAmResponsibleAction(clusterState: ClusterState) = {
+		clusterState.sortedUpAndReachable.head.uniqueAddress == cluster.selfAddress
+	}
 
-  override def postStop () = {
-    super.postStop ()
-    unreachableTimer.foreach(_.cancel())
-  }
+	override def postStop() = {
+		super.postStop()
+		unreachableTimer.foreach(_.cancel())
+	}
 }
--- src/main/scala/com/ajjpj/simpleakkadowning/SurvivalDecider.scala
+++ src/main/scala/com/ajjpj/simpleakkadowning/SurvivalDecider.scala
@@ -1,99 +1,95 @@
 package com.ajjpj.simpleakkadowning
 
 import akka.actor.Address
-import akka.cluster.{Member, UniqueAddress}
+import akka.cluster.{ Member, UniqueAddress }
 import com.ajjpj.simpleakkadowning.SurvivalDecider.ClusterState
 import com.typesafe.config.Config
 
 import scala.collection.Set
 import scala.collection.immutable.SortedSet
 
-
 trait SurvivalDecider {
-  def isInMinority(clusterState: ClusterState, selfAddress: Address): Boolean
+	def isInMinority(clusterState: ClusterState, selfAddress: Address): Boolean
 }
 
 object SurvivalDecider {
-  private val memberOrdering = new Ordering[ClusterMemberInfo] {
-    override def compare (x: ClusterMemberInfo, y: ClusterMemberInfo) =
-      Member.addressOrdering.compare(x.uniqueAddress.address, y.uniqueAddress.address)
-  }
+	private val memberOrdering = new Ordering[ClusterMemberInfo] {
+		override def compare(x: ClusterMemberInfo, y: ClusterMemberInfo) =
+			Address.addressOrdering.compare(x.uniqueAddress, y.uniqueAddress)
+	}
 
-  case class ClusterMemberInfo(uniqueAddress: UniqueAddress, roles: Set[String], member: Member)
-  case class ClusterState(upMembers: Set[ClusterMemberInfo], unreachable: Set[UniqueAddress]) {
-    lazy val sortedUpMembers = SortedSet.empty(memberOrdering) ++  upMembers
-    lazy val sortedUpAndReachable = sortedUpMembers.filterNot (x => unreachable.contains(x.uniqueAddress))
-    lazy val upReachable = upMembers.filterNot(x => unreachable(x.uniqueAddress))
-    lazy val upUnreachable = upMembers.filter(x => unreachable(x.uniqueAddress))
-  }
+	case class ClusterMemberInfo(uniqueAddress: Address, roles: Set[String], member: Member)
+	case class ClusterState(upMembers: Set[ClusterMemberInfo], unreachable: Set[Address]) {
+		lazy val sortedUpMembers = SortedSet.empty(memberOrdering) ++ upMembers
+		lazy val sortedUpAndReachable = sortedUpMembers.filterNot(x => unreachable.contains(x.uniqueAddress))
+		lazy val upReachable = upMembers.filterNot(x => unreachable(x.uniqueAddress))
+		lazy val upUnreachable = upMembers.filter(x => unreachable(x.uniqueAddress))
+	}
 
+	def apply(config: Config): SurvivalDecider = {
+		val cc = config.getConfig("simple-akka-downing")
 
-  def apply(config: Config): SurvivalDecider = {
-    val cc = config.getConfig("simple-akka-downing")
+		cc.getString("active-strategy") match {
+			case "static-quorum" =>
+				val ccc = cc.getConfig("static-quorum")
+				val quorumSize = ccc.getInt("quorum-size")
+				val role = ccc.getString("role") match {
+					case r if r.trim.isEmpty => None
+					case r => Some(r)
+				}
+				new FixedQuorumDecider(quorumSize, role)
+			case "keep-majority" =>
+				val ccc = cc.getConfig("keep-majority")
+				val role = ccc.getString("role") match {
+					case r if r.trim.isEmpty => None
+					case r => Some(r)
+				}
+				new KeepMajorityDecider(role)
+			case "keep-oldest" =>
+				val ccc = cc.getConfig("keep-oldest")
+				val downIfAlone = ccc.getBoolean("down-if-alone")
+				new KeepOldestDecider(downIfAlone)
+		}
+	}
 
-    cc.getString("active-strategy") match {
-      case "static-quorum" =>
-        val ccc = cc.getConfig("static-quorum")
-        val quorumSize = ccc.getInt("quorum-size")
-        val role = ccc.getString("role") match {
-          case r if r.trim.isEmpty => None
-          case r => Some(r)
-        }
-        new FixedQuorumDecider(quorumSize, role)
-      case "keep-majority" =>
-        val ccc = cc.getConfig("keep-majority")
-        val role = ccc.getString("role") match {
-          case r if r.trim.isEmpty => None
-          case r => Some(r)
-        }
-        new KeepMajorityDecider(role)
-      case "keep-oldest" =>
-        val ccc = cc.getConfig("keep-oldest")
-        val downIfAlone = ccc.getBoolean("down-if-alone")
-        new KeepOldestDecider(downIfAlone)
-    }
-  }
+	class FixedQuorumDecider(quorumSize: Int, role: Option[String]) extends SurvivalDecider {
+		override def isInMinority(clusterState: ClusterState, selfAddress: Address) = {
+			val relevantMembers = role match {
+				case Some(r) => clusterState.upMembers.filter(_.roles contains r)
+				case None => clusterState.upMembers
+			}
 
+			(relevantMembers -- clusterState.upUnreachable).size < quorumSize
+		}
+	}
 
-  class FixedQuorumDecider(quorumSize: Int, role: Option[String]) extends SurvivalDecider {
-    override def isInMinority(clusterState: ClusterState, selfAddress: Address) = {
-      val relevantMembers = role match {
-        case Some (r) => clusterState.upMembers.filter (_.roles contains r)
-        case None =>     clusterState.upMembers
-      }
+	class KeepMajorityDecider(role: Option[String]) extends SurvivalDecider {
+		override def isInMinority(clusterState: ClusterState, selfAddress: Address) = {
+			role match {
+				case Some(r) =>
+					val all = clusterState.upMembers.filter(_.roles contains r)
+					val unreachable = clusterState.upUnreachable.filter(_.roles contains r)
+					all.size <= 2 * unreachable.size
+				case None =>
+					clusterState.upMembers.size <= 2 * clusterState.upUnreachable.size
+			}
+		}
+	}
 
-      (relevantMembers -- clusterState.upUnreachable).size < quorumSize
-    }
-  }
+	class KeepOldestDecider(downIfAlone: Boolean) extends SurvivalDecider {
+		override def isInMinority(clusterState: ClusterState, selfAddress: Address) = {
+			val allRelevant = clusterState.upMembers
+			val oldestRelevant = allRelevant.foldLeft(allRelevant.head)((a, b) => if (a.member isOlderThan b.member) a else b)
 
-  class KeepMajorityDecider(role: Option[String]) extends SurvivalDecider {
-    override def isInMinority (clusterState: ClusterState, selfAddress: Address) = {
-      role match {
-        case Some(r) =>
-          val all = clusterState.upMembers.filter(_.roles contains r)
-          val unreachable = clusterState.upUnreachable.filter(_.roles contains r)
-          all.size <= 2*unreachable.size
-        case None =>
-          clusterState.upMembers.size <= 2*clusterState.upUnreachable.size
-      }
-    }
-  }
-
-  class KeepOldestDecider(downIfAlone: Boolean) extends SurvivalDecider {
-    override def isInMinority (clusterState: ClusterState, selfAddress: Address) = {
-      val allRelevant = clusterState.upMembers
-      val oldestRelevant = allRelevant.foldLeft(allRelevant.head)((a, b) => if (a.member isOlderThan b.member) a else b)
-
-      if (downIfAlone) {
-        clusterState.upReachable match {
-          case s if s == Set(oldestRelevant) => true                                       // only the oldest node --> terminate
-          case _ if clusterState.unreachable == Set(oldestRelevant.uniqueAddress) => false // the oldest node is the only unreachable node --> survive
-          case _ => clusterState.unreachable contains oldestRelevant.uniqueAddress
-        }
-      }
-      else {
-        clusterState.unreachable contains oldestRelevant.uniqueAddress
-      }
-    }
-  }
+			if (downIfAlone) {
+				clusterState.upReachable match {
+					case s if s == Set(oldestRelevant) => true // only the oldest node --> terminate
+					case _ if clusterState.unreachable == Set(oldestRelevant.uniqueAddress) => false // the oldest node is the only unreachable node --> survive
+					case _ => clusterState.unreachable contains oldestRelevant.uniqueAddress
+				}
+			} else {
+				clusterState.unreachable contains oldestRelevant.uniqueAddress
+			}
+		}
+	}
 }
